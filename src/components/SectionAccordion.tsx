2b1df8a6fce25edfb16f9cd6dd58d82da06afc56
npc <creative.minarai@gmail.com>
2025-03-01 14:41:54 +0900

    未使用のインポートを削除

--- src/components/SectionAccordion.tsx
+++ src/components/SectionAccordion.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useRef } from 'react';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import remarkBreaks from 'remark-breaks';
